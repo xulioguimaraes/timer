5b90ca5354fb9b56dc8ff0b0dccbf13a3d70a027
Julio <despairnatural@gmail.com>
2022-08-04 19:35:52 -0400

    Localização

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { Timer } from "./pages/Timer";
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-
+import ptLocale from 'date-fns/locale/pt-BR'
 
 function App() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptLocale}>
       <Timer />
     </LocalizationProvider>
   );
